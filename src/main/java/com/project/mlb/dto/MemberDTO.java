a0e786828c19bb2c1cced26e24471dde31b31dfa
HiiWee <wpdlzhf159@gmail.com>
2022-08-08 00:14:34 +0900

    Add: 이전 변경 사항 재 적용
    스프링 버전 변경
    MemberDTO 필드명 변경

--- src/main/java/com/project/mlb/dto/MemberDTO.java
+++ src/main/java/com/project/mlb/dto/MemberDTO.java
@@ -4,19 +4,19 @@ import lombok.Getter;
 
 @Getter
 public class MemberDTO {
-    private String login_id;
+    private String loginId;
     private String password;
     private String name;
     private String nickname;
     private String email;
-    private String phone_number;
+    private String phoneNumber;
 
-    public MemberDTO(String login_id, String password, String name, String nickname, String email, String phone_number) {
-        this.login_id = login_id;
+    public MemberDTO(String loginId, String password, String name, String nickname, String email, String phoneNumber) {
+        this.loginId = loginId;
         this.password = password;
         this.name = name;
         this.nickname = nickname;
         this.email = email;
-        this.phone_number = phone_number;
+        this.phoneNumber = phoneNumber;
     }
 }
